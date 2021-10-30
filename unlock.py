8c1a229ebb053e9f1ef91ddfb038dbb2945a7d81
huoyiming <dghuoyiming@163.com>
2021-10-30 23:32:39 +0800

    Add Backup Function
    增加备份功能和注释

--- unlock.py
+++ unlock.py
@@ -1,17 +1,17 @@
 from __future__ import print_function
 import ctypes, sys
-
+##获取管理员权限
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
 if is_admin():
-    with open('etc/original','r',encoding='utf-8') as f:
-        data = f.read()
+    with open('etc/origin','r',encoding='utf-8') as f:
+        data = f.read()##读取备份的hosts
     print(data)
     with open('C:/Windows/System32/drivers/etc/hosts','w',encoding='utf-8') as j:
-        new = j.write(data)
+        new = j.write(data)##写入系统hosts
 
 else:
     if sys.version_info[0] == 3:
